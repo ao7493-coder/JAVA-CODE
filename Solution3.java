2b2bbf28f7a627eae848357b3bb99d4f15b97838
ANKIT <ao7493@srmist.edu.in>
2026-08-18 16:02:38 +0530

    Two Sum
    Two Sum of easy level with target

---
+++ Solution3.java
@@ -0,0 +1,13 @@
+class Solution3 {
+    public int[] twoSum(int[] nums, int target) {
+        for ( int i = 0 ; i < num.length ; i ++ ) { 
+            for ( int j = i + 1 ; j < nums.length ; j++ ) { 
+                if ( nums[i] + nums[j] = targent ) { 
+                    return new int[] = {i,j}; 
+
+                }
+            }
+        }
+        return new int[] {} ; 
+    }
+}
